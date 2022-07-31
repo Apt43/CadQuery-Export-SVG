b81efdd5620381d3b5c7414755d2eb248c74c769
Eric Chen <globate@hotmail.com>
2022-07-31 14:18:23 -0700

    Finalize ExportSvg.py.

--- ExportSvg.py
+++ ExportSvg.py
@@ -8,9 +8,11 @@
 import cadquery as cq
 from cadquery import exporters
 
+# exportSvg:    exports a Workplane object to SVG image file.
 # result:   Workplane object to extport.
-# filePath: Full path to export the svg file to.
-# for other parameters, see CadQuery official documentation on export to SVG.
+# filePath: Full path to export the SVG file to.
+# For other parameters, see CadQuery official documentation on Export to SVG.
+# 'width' is fixed to 500. 'height' is calculated from object size.
 def exportSvg(
     result,
     filePath,
@@ -20,57 +22,43 @@ def exportSvg(
     hiddenColor = (0, 0, 255),
     showHidden = True):
 
-    s = cq.Workplane()
-    # need to get a single solid, otherwise findSolids will return compound
-    s = s.union(result)
+    r = cq.Workplane()
 
     # === determine object size ===
-    # --- rotate to get the size at view angle ---
-    s = s.rotate((0, 0, 0), (0, 0, 1), -45)
-    s = s.rotate((0, 0, 0), (0, 1, 1), 45)
+    # need to get a single solid, otherwise findSolids will return compound object
+    r = r.union(result)
+
+    # --- rotate to get the size at viewing angle ---
+    r = r.rotate((0.0, 0.0, 0.0), (0.0, 0.0, 1.0), -45.0)
+    r = r.rotate((0.0, 0.0, 0.0), (1.0, 0.0, 0.0), 45.0)
 
     # --- ---
-    s = s.findSolid()
-    s = s.BoundingBox() # returns BoundBox()
+    r = r.findSolid()
+    r = r.BoundingBox() # returns BoundBox()
 
     # xmax, xmin, ... values are not in the official document,
-    # found in CadQuery source code
-    width = s.xmax - s.xmin
-    height = s.zmax - s.zmin
+    # these values are found in CadQuery source code
+    width = r.xmax - r.xmin
+    height = r.zmax - r.zmin
+
+    # === calculate height ===
+    imageWidth = 500.0
+    imageHeight = imageWidth * height / width
 
     # === ===
-    printWidth = 500.0
-    printHeight = printWidth * height / width
-
     # do not change width, use 500.
-    # do not change projectionDir, work properly with the rotated object for export
-    # color can be changed freely.
+    # do not change projectionDir, it works properly with the rotated object for export.
     optSvg = {
-        'width': printWidth,
-        'height': printHeight,
+        'width': imageWidth,
+        'height': imageHeight,
         'marginLeft': marginLeft,
         'marginTop': marginTop,
-        'projectionDir': (1, 1, 1),
+        'projectionDir': (1.0, 1.0, 1.0),
         'strokeWidth': 0.05,
         'strokeColor': strokeColor,
         'hiddenColor': hiddenColor,
         'showHidden': showHidden}
 
-    # must rotate to get the projection align with displayed on screen
-    r = result.rotate((0.0, 0.0, 0.0), (1.0, 0.0, 0.0), -90)
+    # must rotate to get the projection the same as displayed on screen
+    r = result.rotate((0.0, 0.0, 0.0), (1.0, 0.0, 0.0), -90.0)
     exporters.export(r, filePath, opt = optSvg)
-
-# === test ===
-if __name__ == 'temp':
-    r = cq.Workplane()
-    r = r.box(8.0, 8.0, 1.0)
-    r = r.faces('>Z')
-    r = r.workplane()
-    r = r.moveTo(1.0, 1.0)
-    r = r.polygon(5, 3.0)
-    r = r.extrude(2.0)
-
-    show_object(r)
-
-    filePath = 'D:\\Temp\\ExportSVG1.svg'
-    exportSvg(r, filePath)
